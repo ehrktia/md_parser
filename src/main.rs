75c519470860386d0307aaea09a7cb867c03f813
karthick <a.karthie+git@pm.me>
2025-09-02 20:59:05 +0100

    feat(file): read file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod file_read;
 mod syntax_map;
 
+use std::path::Path;
+
 use crate::syntax_map::MemStore;
 fn main() {
     let mut store = syntax_map::MemStore::new();
     MemStore::initialize(&mut store);
-    let file_path = "../docs/ma_parser.md";
-    let data = file_read::read_file(file_path).unwrap();
-    println!("file data:{:?}", data.to_vec());
+    let file_path = Path::new("./docs/ma_parser.md").to_str().unwrap();
+    // TODO: usedata to build a lexical pair on each word
+    let _data = file_read::read_file(file_path).unwrap();
 }
